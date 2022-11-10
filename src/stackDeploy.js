b5be6c16db08078065059909da6ca2be108a7026
Konlawat Kaewkan <nohrsom@Konlawats-MacBook-Pro.local>
2022-11-10 13:01:01 +0700

    Update docker service image instead of doing docker overwrite file.

--- src/stackDeploy.js
+++ src/stackDeploy.js
@@ -2,7 +2,43 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
-async function stackDeploy(folder, stack) {
+function waitmS(mS) {
+   return new Promise((resolve) => {
+      setTimeout(() => {
+         resolve();
+      }, mS);
+   });
+}
+
+async function isServiceUp(keywordService) {
+   let output = "";
+
+   await exec.exec(
+      `bash -c "docker service ls | grep ${keywordService} | awk '{print $4}'"`,
+      [],
+      {
+         listeners: {
+            stdout: (data) => {
+               output += data.toString();
+            },
+         },
+      }
+   );
+
+   return output.includes("1/1");
+}
+
+function waitServiceUp(keywordService) {
+   return new Promise(async (resolve) => {
+      while (!(await isServiceUp(keywordService))) await waitmS(1000);
+
+      await waitmS(5000);
+
+      resolve();
+   });
+}
+
+async function stackDeploy(folder, stack, images = []) {
    core.startGroup("Deploy the Stack");
    const opts = [
       "-c",
@@ -10,14 +46,23 @@ async function stackDeploy(folder, stack) {
       "-c",
       "docker-compose.override.yml",
       "-c",
-      "compose.override.yml",
-      "-c",
       "./test/setup/ci-test.overide.yml",
       stack,
    ];
    await exec.exec("docker stack deploy", opts, { cwd: `./${folder}` });
 
+   await waitServiceUp("sails");
+
+   for (let i = 0; i < images.length; i++) {
+      const shortName = images[i].replace("ab_service_", "");
+
+      await exec.exec(
+         `docker service update --image ${images[i]}:test ${stack}_${shortName}`
+      );
+   }
+
    await exec.exec("docker stack services", [stack]);
+
    core.endGroup();
 }
 module.exports = stackDeploy;
