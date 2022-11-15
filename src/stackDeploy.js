d4b2dc7a237011a22693b0a7868decc1e150c0ed
Nate <7259@pm.me>
2022-11-15 15:29:40 +0700

    move code for easier testing

--- src/stackDeploy.js
+++ src/stackDeploy.js
@@ -1,38 +1,7 @@
 // docker stack deploy -c $File -c docker-compose.override.yml $TestOveride ab
 const core = require("@actions/core");
 const exec = require("@actions/exec");
-
-function waitmS(mS) {
-   return new Promise((resolve) => {
-      setTimeout(() => {
-         resolve();
-      }, mS);
-   });
-}
-
-async function isServiceUp(keywordService) {
-   let output = "";
-
-   await exec.exec(
-      `bash -c "docker service ls | grep ${keywordService} | awk '{print $4}'"`,
-      [],
-      {
-         listeners: {
-            stdout: (data) => {
-               output += data.toString();
-            },
-         },
-      }
-   );
-
-   return output.includes("1/1");
-}
-
-async function waitServiceUp(keywordService) {
-   while (!(await isServiceUp(keywordService))) await waitmS(1000);
-
-   await waitmS(5000);
-}
+const { waitServiceUp } = require("./util/waitService.js");
 
 async function stackDeploy(folder, stack, images = []) {
    core.startGroup("Deploy the Stack");
